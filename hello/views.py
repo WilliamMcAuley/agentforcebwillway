381da397a4d64fb4f38ecd87fec8cac6e48416ad
William McAuley <bwillway@outlook.com>
2025-04-17 14:40:28 -0400

    updated imports to get view.py to work again

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,6 @@
-# from django.shortcuts import render
-import os
-from django.http import HttpResponse
+from django.shortcuts import render
+# import os
+# from django.http import HttpResponse
 
 from .models import Greeting
 
