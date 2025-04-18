943714eda25f1327ca458e87674a72cd050d2ef9
William McAuley <bwillway@outlook.com>
2025-04-17 22:43:27 -0400

    added account detail and href url for accounts

--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
-# import os
-# from django.http import HttpResponse
+from django.shortcuts import get_object_or_404,render,redirect
 
-# from .models import Greeting,Account
 from .models import Account
+from .forms import AccountForm
+
 
 # Create your views here.
+def account_detail(request,account_id):
+     account = get_object_or_404(Account, id=account_id)
 
+     if request.method == "Post":
+          form = AccountForm(request.POST, instance=account)
+          if form.is_valid():
+               form.save()
+               return redirect("account_detail", account_id=account.id)
+     else:
+          form = AccountForm(instance=account)
+
+     return render(request, "account_detail.html", {"form": form,"account": account})
+#orginal render only views
 def index(request):
      return render(request,"index.html")
 
