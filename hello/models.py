104f4b2bb495c09090d0acfc433134eab32340d2
William McAuley <bwillway@outlook.com>
2025-04-17 21:33:58 -0400

    adding extra fields to the account db page

--- hello/models.py
+++ hello/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Account(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     name = models.CharField(max_length=255)
+    industry = models.CharField(max_length=255)
 
     class Meta:
         managed = False  # Because Heroku Connect creates this table, not Django
