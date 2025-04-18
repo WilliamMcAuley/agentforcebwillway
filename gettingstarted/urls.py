4d6f491152d0fc5af997fb1a27b381cd40d32d18
William McAuley <bwillway@outlook.com>
2025-04-17 23:05:55 -0400

    fixing missing command for account detail error

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -23,7 +23,7 @@ import hello.views
 urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
-    path("accounts/<int:account_id>/", hello.views.account_detail, name="account_detail")
+    path("accounts/<int:account_id>/", hello.views.account_detail, name="account_detail"),
     # Uncomment this and the entry in `INSTALLED_APPS` if you wish to use the Django admin feature:
     # https://docs.djangoproject.com/en/5.2/ref/contrib/admin/
     path("admin/", admin.site.urls)]
