290611fe0c8d708be02304ec212d7e5ded18e706
William McAuley <bwillway@outlook.com>
2025-04-18 16:31:12 -0400

    updated forms.py to show list of fields because they were getting overwritten and only showing the last one and also made the id field read only

--- hello/forms.py
+++ hello/forms.py
@@ -4,6 +4,8 @@ from .models import Account
 class AccountForm(forms.ModelForm):
     class Meta:
         model = Account
-        fields = ['name']  # Add more fields if needed
-        fields = ['industry']  # Add more fields if needed
-        fields = ['id']  # Add more fields if needed
+        fields = ['name', 'industry']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['id'] = forms.IntegerField(initial=self.instance.id, disabled=True)
