58585c83ff27670949a783a73ea152976f1639c9
William McAuley <bwillway@outlook.com>
2025-04-17 21:02:11 -0400

    15th try removed systemmoodstamp but now it found the database

--- hello/models.py
+++ hello/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Account(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     name = models.CharField(max_length=255)
-    system_modstamp = models.DateTimeField()
 
     class Meta:
         managed = False  # Because Heroku Connect creates this table, not Django
