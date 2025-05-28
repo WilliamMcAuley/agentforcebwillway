98880586f83ecac5ce9e2f693d0ff23edc0c7bc4
William McAuley <bwillway@outlook.com>
2025-05-27 21:01:40 -0400

    changed field from stage to application_status__c

--- hello/models.py
+++ hello/models.py
@@ -12,7 +12,7 @@ class Account(models.Model):
 class JobApplication(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     name = models.CharField(max_length=255)
-    stage = models.CharField(max_length=255)
+    Application_Status__c = models.CharField(max_length=255)
     # Add other fields as needed.
 
     class Meta:
