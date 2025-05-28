6204713d4de1a0e6a5759ccfe79d7f6cfd72b528
William McAuley <bwillway@outlook.com>
2025-05-27 22:16:07 -0400

    fixed upercase of db_column

--- python-getting-started/hello/models.py
+++ python-getting-started/hello/models.py
@@ -12,7 +12,7 @@ class Account(models.Model):
 class JobApplication(models.Model):
     id = models.CharField(max_length=255, primary_key=True)
     name = models.CharField(max_length=255)
-    application_status = models.CharField(max_length=255, db_column='Application_Status__c')
+    application_status = models.CharField(max_length=255, db_column='application_status__c')
     # Add other fields as needed.
 
     class Meta:
