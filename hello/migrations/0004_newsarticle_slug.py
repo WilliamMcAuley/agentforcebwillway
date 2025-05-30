a605d9736d9bf4da8b55ee18cea3ce16663ac362
William McAuley <bwillway@outlook.com>
2025-05-30 01:50:08 -0400

    updating blanks

--- hello/migrations/0004_newsarticle_slug.py
+++ hello/migrations/0004_newsarticle_slug.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='newsarticle',
             name='slug',
-            field=models.SlugField(blank=True, unique=True),
+            field=models.SlugField(blank=True, null=True),  # <-- add null=True for safety
         ),
     ]
