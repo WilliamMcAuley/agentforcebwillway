be4e8652fe4658aa4942093634d2b26357560588
William McAuley <bwillway@outlook.com>
2025-05-30 01:29:26 -0400

    updating the model and news html file to add slugs and fix the 500 error when you try to see the list

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor.fields import RichTextField
+from django.utils.text import slugify
 
 class Account(models.Model):
     id = models.IntegerField(primary_key=True)  # or CharField if using Salesforce IDs
@@ -22,9 +23,15 @@ class JobApplication(models.Model):
 
 class NewsArticle(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)  # <-- Add this line
     content = models.TextField()
     published = models.DateTimeField(auto_now_add=True)
     thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)  # Add this line
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
